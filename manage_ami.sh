3db0d474faa76ae2a4eb69501ecde28c2d417ee0
harrislalo <39581698+harrislalo@users.noreply.github.com>
2018-05-24 16:04:24 +0000

    Update manage_ami.sh
    Parsing aws commands output with jq in order to check instance status before stopping and terminating the instance.

--- manage_ami.sh
+++ manage_ami.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
-# manage_ami: - description
-# Usage: manage_ami <region> <ami>
-# Example: manage_ami us-east-1 ami-2f3a6954
+#!/bin/sh                                       
+# manage_ami: - description                     
+# Usage: manage_ami <region> <ami>              
+# Example: manage_ami us-east-1 ami-2f3a6954    
 
 if [ -z "$1" ]
   then
@@ -25,13 +25,28 @@ region=$1
 ami=$2
 
 echo "Deploying AMI "$ami"..."
-aws ec2 run-instances --image-id $ami --region $region > instance_details.txt
-echo "Getting Instance ID..."
-instanceId=`cat instance_details.txt |grep InstanceId |cut -d'"' -f 4`
+instanceId=`aws ec2 run-instances --image-id $ami --region $region |jq '.Instances[0].InstanceId' |tr -d '"'`
+echo "Instance ID:"
 echo $instanceId
 echo "Showing Instance "$instanceId" Attributes..."
 aws ec2 describe-instances --instance-id $instanceId --region $region --output table
+
+#Running Status Code is 16
+echo "Waiting for Instance "$instanceId" to be in running status..."
+instanceStateCode=`aws ec2 describe-instances --instance-id $instanceId --region $region | jq '.Reservations[0].Instances[0].State.Code'`
+while [ $instanceStateCode -ne "16" ]; do
+  sleep 5
+  instanceStateCode=`aws ec2 describe-instances --instance-id $instanceId --region $region | jq '.Reservations[0].Instances[0].State.Code'`
+done
 echo "Stopping Instance "$instanceId"..."
 aws ec2 stop-instances --instance-id $instanceId --region $region
+
+#Stopped Status Code is 80
+echo "Waiting for Instance "$instanceId" to be in stopped status..."
+instanceStateCode=`aws ec2 describe-instances --instance-id $instanceId --region $region | jq '.Reservations[0].Instances[0].State.Code'`
+while [ $instanceStateCode -ne "80" ]; do
+  sleep 5
+  instanceStateCode=`aws ec2 describe-instances --instance-id $instanceId --region $region | jq '.Reservations[0].Instances[0].State.Code'`
+done
 echo "Terminating Instance "$instanceId"..."
 aws ec2 terminate-instances --instance-id $instanceId --region $region
